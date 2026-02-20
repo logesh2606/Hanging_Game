a28ee55224b60be69a1bbdf587fa2d17a576fd87
logesh <logesh26052006@gmail.com>
2026-02-20 22:37:42 +0530

    full_code

--- script.js
+++ script.js
@@ -1,115 +1,188 @@
 mis=[' ','A̲','B̲','C̲','D̲', 'E̲', 'F̲', 'G̲', 'H̲', 'I̲', 'J̲', 'K̲', 'L̲', 'M̲', 'N̲', 'O̲', 'P̲', 'Q̲', 'R̲', 'S̲', 'T̲', 'U̲', 'V̲','W̲','X̲','Y̲','Z̲'];
+alp=[' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 im=["image/st.png","image/st1.png","image/st2.png","image/st3.png","image/st4.png","image/st5.png","image/st6.png"];
 ima=0;
 function au(){
-    di="";
-    for(q of que){
-        if(typeof q==='number'){
-            di=di+'_';
-        }
-        else{
-            di=di+q;
-        }
-    }
-document.getElementById('dis').innerHTML=di;
+    ch='';
+al=0;
+for(qu of a){
+       b=0;
+       for(m of mi){
+              if(m==al){
+                     b=1;
+              }
+       }
+       if(b==1){
+              ch=ch+'_';
+       }
+       else{
+              ch=ch+qu;
+       }
+       al++;
+}
+rle();
+document.getElementById('dis').innerHTML=ch;
 document.getElementById('ig').src=im[ima];
 }
-function wod(a){
-    i=0;
-    for(m of mi){
-        if(m==a){
-            k=0;
-            for(q of que){
-                if(i==q){
-                    que[k]=mis[a];
-                }
-                    k++;}}
-        else{
-            i++;
-        }}
-    if(i==mi.length){
-        ima=(ima+1)%7;
+function wod(idt){
+    ab=[];
+       ac=0;
+       ad=0;
+       for(f of a){
+              if(f==alp[idt]){
+                     ab[ac]=ad;
+                     ac++;
+              }
+              ad++;
+       }
+       if(ac!=0){
+              ac=0;
+       for(ae of ab){
+              if(ch[ae]=='_'){
+                     for(fi of ab){
+                            let ba=0;
+                            for(m of mi){
+                                   if(fi==m){
+                                        a[mi[ba]]='h';
+                                          mi[ba]='-';
+                                          ac=1;
+                                   }
+                                   ba++;
+                            }
+                     }
+              }
+       }
+    }
+    if(ac==0){
+        ima++;
     }
     au();
-    rle();
 }
-function cho(ch){
+function cho(chl){
     ima=0;
     document.getElementById('fr').style.display="block";
     document.getElementById('sm').style.display="none";
-    if(ch==1){
-        q=['F','R',0,1,'H',2,3,'U','I','T',1];
-        m=[5,19,6,18]; 
+    if(chl==1){
+        question=["POMEGRANATE","STRAWBERRY","CANTALOUPE","WATERMELON","GOOSEBERRY","GRAPEFRUIT","PINEAPPLE","RASPBERRY","BLUEBERRY","CRANBERRY"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==2){
-        q=['T',0,1,'N',2,'I',3,'G','P','O',4,'E','R'];
-        m=[8,9,11,14,23];
+    else if(chl==2){
+        question=["DELIBERATION","CONTEMPLATION","RATIOCINATION","INTELLECTION","EXCOGITATION","SPECULATING","CEREBRATION","CONSIDERATION","INTENTNESS","REASONNINGS"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==3){
-        q=['V',0,1,'E','T','A',2,'L','E',3,'A','R',4];
-        m=[5,7,2,6,13];
+    else if(chl==3){
+        question=["CAULIFLOWER","BROCCOLI","CUCUMBER","EGGPLANT","MUSHROOM","ZUCCHINI","ASPARAGUS","ARTICHOKE","BEETROOT","CAPSICUM"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==4){
-        q=['W','I',0,'D',1,2,'I',3,'A',0,'S'];
-        m=[12,1,14,13];
+    else if(chl==4){
+        question=["ALLIGATOR","ARMADILLO","CHAMELEON","CHIMPANZEE","CROCODILE","HEDGEHOG","HIPPOPOTAMUS","ORANGUTAN","PORCUPINE","RHINOCEROS"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==5){
-        q=['D','A',0,1,'Y','A',2,'T',0,'V','I',3,'Y'];
-        m=[9,12,3,20];
+    else if(chl==5){
+        question=["ADMINISTER","COLLABORATE","CONCENTRATE","COMMUNICATE","COORDINATE","DISTRIBUTE","ILLUSTRATE","INVESTIGATE","MANIPULATE","UNDERSTAND"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==6){
-        q=['F',0,1,'I','N',2,3,'I','R',4,'S'];
-        m=[12,25,7,2,4];
+    else if(chl==6){
+        question=["HUMMINGBIRD","NIGHTINGALE","KINGFISHER","WOODPECKER","ROADRUNNER","ALBATROSSES","CASSIOWARY","GUINEAFOWL","SANDPIPERS","SPOONBILLS"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==7){
-        q=['H',0,'U',1,'E','H','O','L',2,3,'T','E',4,'S'];
-        m=[15,19,4,9,13];
+    else if(chl==7){
+        question=["CONGRATULATIONS","ENVIRONMENT","PHOTOGRAPHY","REFRIGERATOR","ARCHITECTURE","COMMUNICATION","SUPERMARKET","THERMOMETER","ELECTRICITY","DIFFERENCE"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==8){
-        q=['H','A',0,0,'Y',1,'E',2,'L',3,'N','G','S'];
-        m=[16,6,5,9];
+    else if(chl==8){
+        question=["AMBIVALENCE","ANTICIPATION","APPREHENSION","BEWILDERMENT","COMPASSION","CONTENTMENT","DETERMINATION","DISAPPOINTMENT","EMBARRASSMENT","FRUSTRATION "];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==9){
-        q=['E','N',0,1,'R',2,'N','M','E',3,'T',4,'A','R','E'];
-        m=[22,9,15,14,3];
+    else if(chl==9){
+        question=["RAINFOREST","WATERFALL","EVERGREEN","ECOSYSTEM","MOUNTAIN","COUNTRYSIDE","LIGHTNING","BIODIVERSITY","PRECIPITATION","WILDERNESS","SUNSHINE","ATMOSPHERE"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
-    else if(ch==10){
-        q=['D',0,'S','T','I','N',1,'T','I',2,3,'C','I',4,5];
-        m=[5,1,15,14,20,25];
+    else if(chl==10){
+        question=["AUSTRALIA","ARGENTINA","SINGAPORE","INDONESIA","VENEZUELA","PHILIPPINES","LUXEMBOURG","BANGLADESH","MADAGASCAR","SWITZERLAND"];
+p=Math.round(Math.random()*(question.length-1));
+a=question[p];
+mi=[];
+r=Math.round((Math.random()*(a.length/2))+1);
+for (let i= 0; i<r; i++) {
+       mi[i]=Math.round(Math.random()*(a.length-1));
+}
     }
     else{
+           document.getElementById('go').style.display="none";
         document.getElementById('sm').style.display="flex";
         document.getElementById('fr').style.display="none";
         document.getElementById('dis').innerHTML="SORRY";
-        document.getElementById('go').style.display="none";
+        mi=[0];
     }
-    que=q;
-    mi=m;
-    res=ch;
     au();
     
 }
 function rle(){
-    a=0;
-    for(q of que){
-        if(typeof q!='number'){
-            a++;
-        }
-    }
-    if(a==que.length){
-        document.getElementById('fr').style.display="none";
-        document.getElementById('go').style.display="flex";
-        document.getElementById('rl').innerHTML="WINNER🏆";
+    if(a==ch){
+       document.getElementById('fr').style.display="none";
+       document.getElementById('go').style.display="flex";
+       document.getElementById('rl').innerHTML="WINNER🏆";
     }
     if(ima==6){
-        document.getElementById('fr').style.display="none";
-        document.getElementById('go').style.display="flex";
-        document.getElementById('rl').innerHTML="Loss😔";
+       document.getElementById('fr').style.display="none";
+       document.getElementById('go').style.display="flex";
+       document.getElementById('rl').innerHTML="Loss😔";
     }
-}
-function re(){
-    document.getElementById('go').style.display="none";
-    cho(res);
 }
 
 
